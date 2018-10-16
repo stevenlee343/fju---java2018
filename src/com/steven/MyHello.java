89da1201b909c5eae374a1295966bcc55c0e873a
user <stxx0304@gmail.com>
2018-10-16 12:07:27 +0800

    java1016

---
+++ src/com/steven/MyHello.java
@@ -0,0 +1,48 @@
+package com.steven;
+
+import com.steven.Personn;
+import com.steven.Studentt;
+
+public class MyHello {
+
+	public static void main(String[] args) {
+		
+		Studentt stu = new Studentt("dss",3,2);
+		stu.name = "dsf";
+		stu.english = 3;
+		stu.math = 3;
+		stu.print();
+		String s1=new String("abc");
+		String s2=new String("abc");
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+		
+		
+		
+		Personn p = new Personn(65.5f, 1.7f);
+//		p.weight = 65.5f;
+//		p.height = 1.7f;
+		System.out.println(p.bmi());
+		p.hello();
+//		Person steven = new Person();
+//		Person jack = null;
+//		jack.hello();
+		/*System.out.println("Hello world");
+		int age = 20;
+		float weight = 65.5f;
+		float height = 1.7f;
+		String name = "steven";
+		int schoolYear = 107;
+		System.out.println(age);
+		System.out.println(weight);
+		System.out.println(height);
+		System.out.println(name);
+		float bmi = weight / (height * height); 
+		System.out.println(bmi);
+		age = age + 1;
+		System.out.println(age);
+		float f = 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f + 0.1f;
+		System.out.println(f);*/
+	}
+
+}
