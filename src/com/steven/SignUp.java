f8ace25942edb370ed738ae264bddce3e9c1c292
user <stxx0304@gmail.com>
2018-10-23 11:40:54 +0800

    RangeTester練習

---
+++ src/com/steven/SignUp.java
@@ -0,0 +1,17 @@
+package com.steven;
+
+import java.util.Scanner;
+
+public class SignUp {
+
+	public static void main(String[] args) {
+		System.out.println("Are you 18 (y/N)");
+		Scanner scanner = new Scanner(System.in);
+		String answer = scanner.nextLine();
+		boolean adult = answer.equalsIgnoreCase(answer);
+		
+		
+				
+	}
+
+}
